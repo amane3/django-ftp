bc36845003400155760aad66368acce89635c976
amane3 <38863546+amane3@users.noreply.github.com>
2019-04-23 22:12:51 +0900

    Update models.py

--- upload_form/models.py
+++ upload_form/models.py
@@ -63,23 +63,3 @@ def send_email(sender, instance, **kwargs):
               'token':PasswordResetTokenGenerator().make_token(instance),
               })
               instance.email_user(subject,message)
-#@receiver(post_save,sender=UserProfile)
-#def send_user_data_when_created_by_admin(sender, instance, **kwargs):
-#	first_name = instance.user.first_name
-#        print('first name is',first_name)
-#        last_name = instance.user.last_name
-#        email = instance.user.email
-#        html_content = "your first name:%s <br> last name:%s "
-#        message=EmailMessage(subject='welcome',body=html_content %(first_name,last_name),to=[email])
-#        message.content_subtype='html'
-#	message.send()
-#class UserProfile(models.Model):
-	#address = models.CharField(max_length=300,blank=True,null=True,help_text=("enter the address"))
-	#contact = models.CharField(max_length=100,blank=True,null=True,help_text=("enter the contact"))
-	#user = models.ForeignKey(User)
-       
-
-
-#def __str__(self):
-#	return (self.username)
-
